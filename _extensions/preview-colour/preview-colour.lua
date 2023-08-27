af6100ba02e884c72231b081b25d7669f1278334
Mickaël Canouil <8896044+mcanouil@users.noreply.github.com>
2023-08-27 22:37:28 +0200

    refactor: remove unused code and improve code to parse everywhere (#6)

--- _extensions/preview-colour/preview-colour.lua
+++ _extensions/preview-colour/preview-colour.lua
@@ -22,92 +22,16 @@
 # SOFTWARE.
 ]]
 
-function get_colour(element)
-  local hex = element.text:match('(#' .. string.rep('[0-9a-fA-F]', 6) .. ')')
-  if hex == nil then
-    hex = element.text:match('(rgb%s*%(%s*%d+%s*,%s*%d+%s*,%s*%d+%s*%))')
-  end
-  if hex == nil then
-    hex = element.text:match('(hsl%s*%(%s*%d+%s*,%s*%d+%s*%%,%s*%d+%s*%%s*%))')
-  end
-
-  return hex
-end
-
-function process_str(element)
-  hex = get_colour(element)
-  if hex ~= nil then
-    if quarto.doc.is_format("html:js") then
-      colour_preview_mark = "<span style=\"display: inline-block; color: " .. hex .. ";\">&#9673;</span>"
-      new_text = string.gsub(
-        element.text,
-        hex,
-        hex .. colour_preview_mark
-      )
-      return pandoc.RawInline('html', new_text)
-    elseif quarto.doc.is_format("latex") then
-      colour_preview_mark = "\\textcolor[HTML]{" .. string.gsub(hex, '#', '') .. "}{\\textbullet}"
-      new_text = string.gsub(
-        element.text,
-        hex,
-        "\\" .. hex .. colour_preview_mark
-      )
-      return pandoc.RawInline('latex', new_text)
-    end
-  end
-end
-
-function process_code(element)
-  hex = get_colour(element)
-  if hex ~= nil then
-    if quarto.doc.is_format("html:js") then
-      colour_preview_mark = "<span style=\"display: inline-block; color: " .. hex .. ";\">&#9673;</span>"
-      return pandoc.Span({element, pandoc.RawInline('html', colour_preview_mark)})
-    elseif quarto.doc.is_format("latex") then
-      colour_preview_mark = "\\textcolor[HTML]{" .. string.gsub(hex, '#', '') .. "}{\\textbullet}"
-      return pandoc.Span({element, pandoc.RawInline('latex', colour_preview_mark)})
-    end
-  end
-end
-
-function process_element(element, meta)
-  if element.t == 'Str' and meta['preview-colour']['text'] == true then
-    return process_str(element)
-  elseif element.t == 'Code' and meta['preview-colour']['code'] == true then
-    return process_code(element)
-  elseif element.t == 'Para' or element.t == 'Plain' then
-    new_content = {}
-    for i, child in ipairs(element.content) do
-      new_child = process_element(child, meta)
-      if new_child ~= nil then
-        table.insert(new_content, new_child)
-      else
-        table.insert(new_content, child)
-      end
-    end
-    element.content = new_content
-    return element
-  elseif element.t == 'BulletList' then
-    quarto.log.output("In BulletList")
-    new_content = {}
-    for i, child in ipairs(element.content) do
-      new_child = process_element(child, meta)
-      if new_child ~= nil then
-        table.insert(new_content, new_child)
-      else
-        table.insert(new_content, child)
-      end
-    end
-    element.content = new_content
-    return element
-  end
-end
-
 local function is_empty(s)
   return s == nil or s == ''
 end
 
-function set_colour_preview_meta(meta)
+local preview_colour_meta = {
+  ["text"] = true,
+  ["code"] = true
+}
+
+function get_colour_preview_meta(meta)
   local preview_colour_text = true
   local preview_colour_code = true
   if not is_empty(meta['preview-colour']) then
@@ -122,18 +46,85 @@ function set_colour_preview_meta(meta)
     ["text"] = preview_colour_text,
     ["code"] = preview_colour_code
   }
+  preview_colour_meta = meta['preview-colour']
   return meta
 end
 
-return {
-  {Meta = set_colour_preview_meta},
-  {Pandoc = function (doc)
-    meta = doc.meta
-    for i, block in ipairs(doc.blocks) do
-      if block.t ~= 'CodeBlock' or block.t ~= 'Div' then
-        process_element(block, meta)
+
+function get_colour(element)
+  function get_hex_color(n)
+    return '#' .. string.rep('[0-9a-fA-F]', n)
+  end
+
+  local hex = nil
+  for i = 6, 3, -1 do
+    hex = element.text:match('(' .. get_hex_color(i) .. ')')
+    if (i == 5 or i == 4) and hex ~= nil then
+      hex = nil
+      break
+    end
+    if hex ~= nil and (i == 6 or i == 3) then
+      break
+    end
+  end
+  if hex == nil then
+    hex = element.text:match('(rgb%s*%(%s*%d+%s*,%s*%d+%s*,%s*%d+%s*%))')
+  end
+  if hex == nil then
+    hex = element.text:match('(hsl%s*%(%s*%d+%s*,%s*%d+%s*%%,%s*%d+%s*%%s*%))')
+  end
+
+  return hex
+end
+
+function process_str(element, meta)
+  if preview_colour_meta['text'] == false then
+    return element
+  end
+  if preview_colour_meta['text'] == true then
+    hex = get_colour(element)
+    if hex ~= nil then
+      if quarto.doc.is_format("html:js") then
+        colour_preview_mark = "<span style=\"display: inline-block; color: " .. hex .. ";\">&#9673;</span>"
+        new_text = string.gsub(
+          element.text,
+          hex,
+          hex .. colour_preview_mark
+        )
+        return pandoc.RawInline('html', new_text)
+      elseif quarto.doc.is_format("latex") then
+        colour_preview_mark = "\\textcolor[HTML]{" .. string.gsub(hex, '#', '') .. "}{\\textbullet}"
+        new_text = string.gsub(
+          element.text,
+          hex,
+          "\\" .. hex .. colour_preview_mark
+        )
+        return pandoc.RawInline('latex', new_text)
       end
     end
-    return doc
-  end}
+  end
+end
+
+function process_code(element)
+  if preview_colour_meta['code'] == false then
+    return element
+  end
+  if preview_colour_meta['code'] == true then
+    hex = get_colour(element)
+    if hex ~= nil then
+      if quarto.doc.is_format("html:js") then
+        colour_preview_mark = "<span style=\"display: inline-block; color: " .. hex .. ";\">&#9673;</span>"
+        return pandoc.Span({element, pandoc.RawInline('html', colour_preview_mark)})
+      elseif quarto.doc.is_format("latex") then
+        colour_preview_mark = "\\textcolor[HTML]{" .. string.gsub(hex, '#', '') .. "}{\\textbullet}"
+        return pandoc.Span({element, pandoc.RawInline('latex', colour_preview_mark)})
+      end
+    end
+  end
+end
+
+return {
+  {Meta = get_colour_preview_meta},
+  {Str = process_str},
+  {Code = process_code}
 }
